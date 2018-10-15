f52586f27ea1197a13f84058b377a9b286c56777
ywwl <mifang@ywwl.com>
2018-10-15 15:24:52 +0800

    sd

--- xzcai-study-web/src/main/java/com/xzcai/study/XzcaiStudyMainApplication.java
+++ xzcai-study-web/src/main/java/com/xzcai/study/XzcaiStudyMainApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author: Mr.Wang
  * @create: 2018-09-29
  **/
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class XzcaiStudyMainApplication {
+public class XzcaiStudyMainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(XzcaiStudyMainApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XzcaiStudyMainApplication.class);
     }
 }
